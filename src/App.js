64d04ae6510227dcc26b57e62f3005654b3a5915
mattia biscontini <mattiabiscontini@gmail.com>
2021-07-05 11:48:17 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState("");
+  const[error, setError] = useState({error: false, message:""});
+  
+
+  const handleFetch = async () =>{
+
+    try{
+      const response = await fetch("https://tronalddump.io/random/quote");
+
+      if (response.status !== 200) {
+        throw new Error("Oops!");
+      }
+      const data = await response.json();
+      setData(data);
+      
+    } catch (error) {
+      setError({error: true, message: error.message})
+     
+    }
+    // fetch("https://tronalddump.io/random/quote")
+    // .then((response) => response.json())
+    // .then((data) => console.log(data));
+ 
+  };
+
+  if (error.error) {
+    return <h1>An error has occured: {error.message}</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tronald Dump Quote Machine</h1>
+      <p>{data.value}</p>
+      <button onClick={handleFetch}>Click</button>
     </div>
-  );
-}
+  )
+};
 
 export default App;
